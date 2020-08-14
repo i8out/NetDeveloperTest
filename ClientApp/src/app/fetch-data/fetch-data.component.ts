af160a039c571ac31854a89f868cb8c0fc68cb85
paulValencia <free_nebula@yahoo.com>
2020-08-14 11:34:04 +0800

    start user manager page

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,21 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UserModel } from '../models/user-model';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public users: UserModel[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<UserModel[]>(baseUrl + 'api/users').subscribe(result => {
+      this.users = result;
     }, error => console.error(error));
   }
-}
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+  editUser(e){
+    console.log(e);
+  }
 }
---
+++ ClientApp/src/app/models/user-model.ts
@@ -0,0 +1,9 @@
+export class UserModel {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    superAdmin: boolean;
+    fullName: string;
+}
