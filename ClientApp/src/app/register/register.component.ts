ae3ccb54987c2de7a642d456348715b74b7a0411
paulValencia <free_nebula@yahoo.com>
2020-08-15 15:25:10 +0800

    version 1 done

--- ClientApp/src/app/register/register.component.ts
+++ ClientApp/src/app/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { UserModel } from '../models/user-model';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators, FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,39 +13,58 @@ export class RegisterComponent implements OnInit {
   @ViewChild('f', { static: false }) form: any;
   user = new UserModel();
 
-  firstName = new FormControl('', [Validators.required]);
-  lastName = new FormControl('', [Validators.required]);
-  password = new FormControl('', [Validators.required]);
-  email = new FormControl('', [Validators.required]);
-  confirmPassword = new FormControl('', [Validators.required]);
+  registerForm: FormGroup = this.formBuilder.group({
+    firstName: [, { validators: [Validators.required], updateOn: "change" }],
+    lastName: [, { validators: [Validators.required], updateOn: "change" }],
+    email: [, { validators: [Validators.required, Validators.email], updateOn: "change", }]
+  });
+
+  passwordsForm: FormGroup = this.formBuilder.group({
+    password: ['', { validators: [Validators.required] }],
+    confirmPassword: ['', { validators: [Validators.required] }]
+  }, { validators: this.passwordConfirming('password', 'confirmPassword') });
+
+  passwordConfirming(passwordKey: string, passwordConfirmationKey: string) {
+    return (group: FormGroup) => {
+      let pass = group.get('password').value;
+      let confirmPass = group.get('confirmPassword').value;
+      return pass === confirmPass ? null : { notSame: true }
+    };
+  }
+
 
   constructor(
+    private formBuilder: FormBuilder,
     private userService: UserService,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private dialogref: MatDialogRef<RegisterComponent>
-  ) { }
+    private dialogref: MatDialogRef<RegisterComponent>) { }
 
   ngOnInit() {
   }
 
   update() {
-    this.user.firstName = this.firstName.value;
-    this.user.lastName = this.lastName.value;
-    this.user.password = this.password.value;
-    this.user.email = this.email.value;
-    this.userService.updateUser(this.user).subscribe((res: any) => {
-    }, (err) => console.error(err), () => this.savingDone());
 
+    this.user.firstName = this.registerForm.get('firstName').value;
+    this.user.lastName = this.registerForm.get('lastName').value;
+    this.user.password = this.passwordsForm.get('password').value;
+    this.user.email = this.registerForm.get('email').value;
+
+    this.userService.getLoginCredentials(this.user).subscribe(result => {
+      if (result) {
+        this.duplicateError();
+      }
+      // else {
+      //   this.userService.updateUser(this.user).subscribe((res: any) => {
+      //   }, (err) => console.error(err), () => this.savingDone());
+      // }
+    }, (err) => this.userService.updateUser(this.user).subscribe((res: any) => { }, (err) => console.error(err), () => this.savingDone()));
   }
 
   savingDone(): void {
     this.dialogref.close('From Dialog');
   }
 
-  checkPasswords() {
-    let firstPassword = this.password.value;
-    let secndPassword = this.confirmPassword.value;
-    return firstPassword === secndPassword ? null : { notSame: true };
+  duplicateError(): void {
+    alert("Email already Registered!");
   }
-
 }
