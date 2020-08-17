5fddc32f2030507e1ab8264b18301e443c65f2cf
paulValencia <free_nebula@yahoo.com>
2020-08-17 10:34:56 +0800

    conext update

--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NetDeveloperTest.Models
 {
-    public class User
+    public partial class User
     {
         public User()
         {
--- Models/User_ProjectContext.cs
+++ Models/User_ProjectContext.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NetDeveloperTest.Models
 {
-    public class User_ProjectContext : DbContext
+    public partial class User_ProjectContext : DbContext
     {
         public User_ProjectContext()
         {
@@ -18,5 +18,42 @@ namespace NetDeveloperTest.Models
         }
 
         public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-9H58DDMB;Database=UserDb;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Email)
+                    .HasName("IX_UserLogin")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
